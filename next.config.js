e0ea481070fd6c56e133b658dea190c900af7411
imchristiandev <imchristiandev@gmail.com>
2023-08-18 20:47:13 -0500

    change of basepath

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
 
  // Add basePath
-  basePath: '/',
+  basePath: 'https://cryptospace.global/',
   trailingSlash: true,
   output: 'export',
   env: {
