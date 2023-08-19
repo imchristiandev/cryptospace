15e8554ae42fbf8b97539aab941f7e915262243c
imchristiandev <imchristiandev@gmail.com>
2023-08-18 20:36:33 -0500

    use of environment variables as next documentation

--- next.config.js
+++ next.config.js
@@ -3,23 +3,19 @@ const nextConfig = {
   reactStrictMode: true,
 
  // Add basePath
-  basePath: './',
+  basePath: '/',
   trailingSlash: true,
   output: 'export',
+  env: {
+    COLLECTION: "ETHEREUM:0xee7f09d6444316ee3df063fa43a5ad9d682a0ca2",
+    API_RARIBLE: "https://api.rarible.org/v0.1",
+    BLOCKCHAIN: "ETHEREUM",
+    API_RARIBLE_KEY: "2f66bcde-3967-407c-b47f-c15ddc9c28eb",
+    TARGET_DATE: "2023-09-01T00:00:00",
+    URL_CONTENT_API: "https://fragosautomotive.com/db-cryptospace/nft.php", //rutas de prueba
+    URL_CONTENT_VIDEO: "https://fragosautomotive.com/multimedia/videos/", //rutas de prueba
+    URL_CONTENT_AUDIO: "https://fragosautomotive.com/multimedia/audios/", //rutas de prueba
+  }
 }
 
-const env = {
-  COLLECTION: "ETHEREUM:0xee7f09d6444316ee3df063fa43a5ad9d682a0ca2",
-  API_RARIBLE: "https://api.rarible.org/v0.1",
-  BLOCKCHAIN: "ETHEREUM",
-  API_RARIBLE_KEY: "2f66bcde-3967-407c-b47f-c15ddc9c28eb",
-  TARGET_DATE: "2023-09-01T00:00:00",
-  URL_CONTENT_API: "https://fragosautomotive.com/db-cryptospace/nft.php", //rutas de prueba
-  URL_CONTENT_VIDEO: "https://fragosautomotive.com/multimedia/videos/", //rutas de prueba
-  URL_CONTENT_AUDIO: "https://fragosautomotive.com/multimedia/audios/", //rutas de prueba
-}
-
-module.exports = {
-  ...nextConfig,
-  env
-}
+module.exports = nextConfig
