801f1687f6e12c1947f933b325064161dd601d73
Enrique Rocha <sistema@powerupsbusiness.com>
2023-08-21 18:35:40 -0600

    Cambios de Ernesto

---
+++ src/app/online-exchange/page.jsx
@@ -0,0 +1,16 @@
+import styles from '@/styles/exchange.module.css'
+
+export default function Exchange() {
+return(
+  <div className={ styles.main }>
+    <div className={ styles.mask }>
+      <h2> Online Exchange</h2>
+      <h3> comming soon..</h3>
+      <img src='/Mockup.png'/>
+      <p>Due to regulatory constraints, our exchange does not accept US citizens. Only non-US citizens are eligible to participate.</p>
+    </div>
+   
+  </div>
+)
+}
+
